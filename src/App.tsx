7c7eb4a11a151ff143d57d971407b3c5b04e0ee7
Etienne Dldc <e.deladonchamps@gmail.com>
2020-03-24 15:42:23 +0100

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ContactItem } from './types';
+import Contact from './Contact';
 
-function App() {
+const App: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [contacts, setContacts] = React.useState<Array<ContactItem>>([
+    {
+      id: '3',
+      name: 'Paul',
+      email: 'paul@gmail.com'
+    },
+    {
+      id: '1',
+      name: 'Alice'
+    },
+    {
+      id: '2',
+      name: 'Bob',
+      email: 'bob@gmail.com'
+    }
+  ]);
+
+  const sortedContact = [...contacts].sort((left, right) => left.name.localeCompare(right.name));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h1 className="title">Contacts</h1>
+      <div className="contacts">
+        {sortedContact.map(contact => {
+          return (
+            <Contact
+              contact={contact}
+              key={contact.id}
+              onDelete={() => {
+                setContacts(contacts.filter(item => item.id !== contact.id));
+              }}
+            />
+          );
+        })}
+      </div>
+      <div className="add">
+        <input value={name} onChange={e => setName(e.target.value)} placeholder="name" />
+        <input value={email} onChange={e => setEmail(e.target.value)} placeholder="email" />
+        <button
+          onClick={() => {
+            const newContact: ContactItem = {
+              id: randomShortId(),
+              name: name,
+              email
+            };
+            const contactsCopy = [...contacts];
+            contactsCopy.push(newContact);
+            setContacts(contactsCopy);
+            setName('');
+            setEmail('');
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
     </div>
   );
+};
+
+/**
+ * Return a short (5 chars) string ID
+ */
+function randomShortId(): string {
+  return Math.random()
+    .toString(36)
+    .substring(7);
 }
 
 export default App;
