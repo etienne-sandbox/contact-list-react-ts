2b1c752a9836f953067fae1e7fe85cc9e5db1d7a
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-09-14 11:25:11 +0200

    Create App

---
+++ src/Contact.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ContactItem } from "./App";
+
+interface Props {
+  contact: ContactItem;
+  onDelete: () => void;
+}
+
+const Contact: React.FC<Props> = ({ contact, onDelete }) => {
+  return (
+    <div className="contact">
+      <div className="infos">
+        <h2>{contact.name}</h2>
+        {contact.email && <p>{contact.email}</p>}
+      </div>
+      <button className="remove" onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
